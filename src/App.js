1ab1924647ce5c664835e0accc6c76c5943444f6
Abishek thiru <101636164+abishek1405@users.noreply.github.com>
2025-07-28 11:43:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import LoginForm from './components/LoginForm'
-import Home from './components/Home'
-import Products from './components/Products'
-import Cart from './components/Cart'
-import NotFound from './components/NotFound'
+import LoginForm from "./components/LoginForm";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+import NotFound from "./components/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-import './App.css'
+import "./App.css";
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/login" component={LoginForm} />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/products" component={Products} />
-      <Route exact path="/cart" component={Cart} />
-      <Route component={NotFound} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/products" component={Products} />
+      <ProtectedRoute exact path="/cart" component={Cart} />
+      <Route path="/not-found" component={NotFound} />
+      <Redirect to="not-found" />
     </Switch>
   </BrowserRouter>
-)
+);
 
-export default App
+export default App;
